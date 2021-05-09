252713e9d8832784868b0a35a695add78af55ae1
qnguy <qtn3@njit.edu>
2021-05-09 15:25:47 -0500

    Added Authorization Middleware to Verify Access Token Against The Auth0 JSON Web Key Set

--- server/app.js
+++ server/app.js
@@ -78,6 +78,26 @@ app.use((req, res, next) => {
     next();
 });
 
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+    // Dynamically provide a signing key
+    // based on the kid in the header and
+    // the signing keys provided by the JWKS endpoint.
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://dev-wxuhoh54.us.auth0.com/.well-known/jwks.json`
+    }),
+
+    // Validate the audience and the issuer.
+    audience: 'http://localhost:3000/api/v1/cities',
+    issuer: [`https://dev-wxuhoh54.us.auth0.com/`],
+    algorithms: ['RS256'],
+});
+
 // Router Mounting
 app.use('/', authRouter);
 
